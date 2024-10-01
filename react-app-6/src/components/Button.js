9ccaca62e42a2f43bf350c057f184da9fb68be0b
Shubham228197 <shubham228pandey@gmail.com>
2024-10-01 23:49:32 +0530

    White Text bug fixed

--- react-app-6/src/components/Button.js
+++ react-app-6/src/components/Button.js
@@ -12,10 +12,8 @@ function Button({
   rounded,
   ...rest
 }) {
-  const classes = className(
-    rest.className,
-    "flex items-center px-3 py-1.5 border",
-    {
+  const classes = twMerge(
+    className(rest.className, "flex items-center px-3 py-1.5 border", {
       "border-blue-500 bg-blue-500 text-white": primary,
       "border-gray-900 bg-gray-900 text-white": secondary,
       "border-green-500 bg-green-500 text-white": success,
@@ -28,7 +26,7 @@ function Button({
       "text-green-500": outline && success,
       "text-yellow-400": outline && warning,
       "text-red-500": outline && danger,
-    }
+    })
   );
 
   return (
