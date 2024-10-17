63c99b86fa84ad8be50acb8aa5cc95b256ceac63
Shubham228197 <shubham228pandey@gmail.com>
2024-10-17 20:18:23 +0530

    comments added

--- react-app-6/src/components/Modal.js
+++ react-app-6/src/components/Modal.js
@@ -1,15 +1,21 @@
 import ReactDOM from "react-dom";
 import { useEffect } from "react";
 
+// actionBar section is for the button action
+// shown at the bottom on modal div
 function Modal({ onClose, children, actionBar }) {
   useEffect(() => {
+    // when Modal is shown the scrolling is stopped
     document.body.classList.add("overflow-hidden");
 
+    // if scrolling not stopped then modal will take 100vw and
+    // 100vh but the content below have not modal effect
     return () => {
       document.body.classList.remove("overflow-hidden");
     };
   }, []);
 
+  // createPortal to render modal outside of the DOM tree
   return ReactDOM.createPortal(
     <div>
       <div
@@ -23,6 +29,7 @@ function Modal({ onClose, children, actionBar }) {
         </div>
       </div>
     </div>,
+    // append modal to the body
     document.querySelector(".modal-container")
   );
 }
--- react-app-6/src/pages/ModalPage.js
+++ react-app-6/src/pages/ModalPage.js
@@ -1,18 +1,24 @@
-import { useState } from 'react';
-import Modal from '../components/Modal';
-import Button from '../components/Button';
+import { useState } from "react";
+import Modal from "../components/Modal";
+import Button from "../components/Button";
 
 function ModalPage() {
+  // State to manage the opening and closing of modal
   const [showModal, setShowModal] = useState(false);
 
+  // Function to open the modal when button is clicked
+  // and close it when it's closed
   const handleClick = () => {
     setShowModal(true);
   };
 
+  // Function to close the modal when it's clicked outside
+  // the modal area or button inside modal is clicked
   const handleClose = () => {
     setShowModal(false);
   };
 
+  // Button to be displayed inside the modal
   const actionBar = (
     <div>
       <Button onClick={handleClose} primary>
@@ -31,8 +37,9 @@ function ModalPage() {
       <Button onClick={handleClick} primary>
         Open Modal
       </Button>
+      {/* check if modal is opened or not to show component */}
       {showModal && modal}
-
+      {/* lorem ipsum to check modal performance when scrolling */}
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. In feugiat elit
         in est interdum fermentum. Cras vehicula a urna sit amet auctor. Mauris
