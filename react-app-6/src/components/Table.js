03952bf3efe441351653ef81bc75c3abf106e71d
Shubham228197 <shubham228pandey@gmail.com>
2024-10-18 23:08:14 +0530

    table implemented with data

--- react-app-6/src/components/Table.js
+++ react-app-6/src/components/Table.js
@@ -1,5 +1,35 @@
-import React from "react";
+function Table({ data, config, keyFn }) {
+  // heading of the table
+  const renderedHeaders = config.map((column) => {
+    return <th key={column.label}>{column.label}</th>;
+  });
 
-export default function Table() {
-  return <div>i am table</div>;
+  const renderedRows = data.map((rowData) => {
+    const renderedCells = config.map((column) => {
+      // will create multiple cells for each row based on the data of array
+      return (
+        <td className='p-2' key={column.label}>
+          {column.render(rowData)}
+        </td>
+      );
+    });
+
+    // will create multiple rows to show cell data
+    return (
+      <tr className='border-b' key={keyFn(rowData)}>
+        {renderedCells}
+      </tr>
+    );
+  });
+
+  return (
+    <table className='table-auto border-spacing-2'>
+      <thead>
+        <tr className='border-b-2'>{renderedHeaders}</tr>
+      </thead>
+      <tbody>{renderedRows}</tbody>
+    </table>
+  );
 }
+
+export default Table;
