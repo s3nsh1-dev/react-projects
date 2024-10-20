a752116bde1dd1587adc1acf74d1f9410c3d397b
Shubham228197 <shubham228pandey@gmail.com>
2024-10-20 22:53:20 +0530

    Cutom Hook Intro

--- react-app-6/src/pages/CounterPage.js
+++ react-app-6/src/pages/CounterPage.js
@@ -1,5 +1,15 @@
-import React from "react";
+import Button from '../components/Button';
+import useCounter from '../hooks/use-counter';
 
-export default function CounterPage() {
-  return <div>Count</div>;
+function CounterPage({ initialCount }) {
+  const { count, increment } = useCounter(initialCount);
+
+  return (
+    <div>
+      <h1>Count is {count}</h1>
+      <Button onClick={increment}>Increment</Button>
+    </div>
+  );
 }
+
+export default CounterPage;
