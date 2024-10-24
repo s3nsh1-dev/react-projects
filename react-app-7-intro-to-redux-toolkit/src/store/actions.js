32d8706ee9d62f2f36fbf3215a754d74005c4418
Shubham228197 <shubham228pandey@gmail.com>
2024-10-24 23:34:30 +0530

    kinda hook

--- react-app-7-intro-to-redux-toolkit/src/store/actions.js
+++ react-app-7-intro-to-redux-toolkit/src/store/actions.js
@@ -1,3 +1,6 @@
 import { createAction } from "@reduxjs/toolkit";
 
+// Define the action creator for resetting the app state
+// this type does not belong to any current state
+// to use this you must mention type = "app/reset" to extraReducer inside your slice object
 export const reset = createAction("app/reset");
