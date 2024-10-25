724e563869284da1a9a7d0f1520aeb0353baeab8
Shubham228197 <shubham228pandey@gmail.com>
2024-10-25 18:28:53 +0530

    CRA

---
+++ react-app-8-manage-redux-slices/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+// import { BrowserRouter, Routes, Route } from "react-dom";
+
+export default function App() {
+  return (
+    <div className='p-4'>
+      <h1 className='text-3xl font-bold text-blue-600'>Hello Tailwind CSS!</h1>
+    </div>
+  );
+}
---
+++ react-app-8-manage-redux-slices/src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+const divRoot = document.getElementById("root");
+const root = ReactDOM.createRoot(divRoot);
+root.render(
+  <>
+    <App />
+  </>
+);
