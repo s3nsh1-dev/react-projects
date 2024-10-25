ffb5c51668b48325763aefde6aa9a358b5ea52f5
Shubham228197 <shubham228pandey@gmail.com>
2024-10-25 23:25:33 +0530

    good approach

--- react-app-8-manage-redux-slices/src/index.js
+++ react-app-8-manage-redux-slices/src/index.js
@@ -1,11 +1,16 @@
 import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
 import App from "./App";
 import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 const divRoot = document.getElementById("root");
 const root = ReactDOM.createRoot(divRoot);
 root.render(
-  <>
-    <App />
-  </>
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
 );
