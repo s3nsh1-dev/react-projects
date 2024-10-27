81bc0ddc5722e7737845be2a68efba0f72329858
Shubham228197 <shubham228pandey@gmail.com>
2024-10-27 20:40:33 +0530

    cra files

--- react-app-9-redux-api-async-thunks/src/App.js
+++ react-app-9-redux-api-async-thunks/src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
-
+import UsersList from "./components/UsersList";
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div className='container mx-auto'>
+      <UsersList />
+    </div>
+  );
 }
