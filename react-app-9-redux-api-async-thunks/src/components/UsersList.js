a54ebf678626b0e9ef3a45d281115588d0542c17
Shubham228197 <shubham228pandey@gmail.com>
2024-10-28 22:41:38 +0530

    1st req. made

--- react-app-9-redux-api-async-thunks/src/components/UsersList.js
+++ react-app-9-redux-api-async-thunks/src/components/UsersList.js
@@ -1,5 +1,13 @@
 import React from "react";
+import { useEffect } from "react";
+// import { useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
+import { fetchUsers } from "../store";
 
 export default function UsersList() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchUsers()); // fetch users when the component mounts
+  }, []);
   return <div>UsersList</div>;
 }
