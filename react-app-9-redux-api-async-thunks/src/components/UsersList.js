5bc7bb8dec66622537bd3aa6126d78a8fd11a75f
Shubham228197 <shubham228pandey@gmail.com>
2024-10-28 23:46:20 +0530

    Homepage

--- react-app-9-redux-api-async-thunks/src/components/UsersList.js
+++ react-app-9-redux-api-async-thunks/src/components/UsersList.js
@@ -1,13 +1,50 @@
-import React from "react";
 import { useEffect } from "react";
-// import { useDispatch } from "react-redux";
-import { useDispatch } from "react-redux";
-import { fetchUsers } from "../store";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers, addUser } from "../store";
+import Button from "./Button";
+import Skeleton from "./Skeleton";
 
-export default function UsersList() {
+function UsersList() {
   const dispatch = useDispatch();
+  const { isLoading, data, error } = useSelector((state) => {
+    return state.users;
+  });
+
   useEffect(() => {
-    dispatch(fetchUsers()); // fetch users when the component mounts
-  }, []);
-  return <div>UsersList</div>;
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
+  const handleUserAdd = () => {
+    dispatch(addUser());
+  };
+
+  if (isLoading) {
+    return <Skeleton times={6} className='h-10 w-full' />;
+  }
+
+  if (error) {
+    return <div>Error fetching data...</div>;
+  }
+
+  const renderedUsers = data.map((user) => {
+    return (
+      <div key={user.id} className='mb-2 border rounded'>
+        <div className='flex p-2 justify-between items-center cursor-pointer'>
+          {user.name}
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div className='flex flex-row justify-between m-3'>
+        <h1 className='m-2 text-xl'>Users</h1>
+        <Button onClick={handleUserAdd}>+ Add User</Button>
+      </div>
+      {renderedUsers}
+    </div>
+  );
 }
+
+export default UsersList;
