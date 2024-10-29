b817042d40d4a9a99ddb3ac42acba3ff08315b67
Shubham228197 <shubham228pandey@gmail.com>
2024-10-29 21:39:17 +0530

    quotA

--- react-app-9-redux-api-async-thunks/src/store/thunks/addUser.js
+++ react-app-9-redux-api-async-thunks/src/store/thunks/addUser.js
@@ -5,6 +5,7 @@ import { faker } from "@faker-js/faker";
 // it seems like faker package is generating id and adding to the object automatically before returning
 const addUser = createAsyncThunk("users/add", async () => {
   const response = await axios.post("http://localhost:3001/users", {
+    // name is depreciated person is used more
     name: faker.person.fullName(),
   });
 
