d39af9d2a5d93f6111843a0490a5920f4852453f
Shubham228197 <shubham228pandey@gmail.com>
2024-11-02 17:27:52 +0530

    vide 377

--- react-app-9-redux-api-async-thunks/src/components/UsersList.js
+++ react-app-9-redux-api-async-thunks/src/components/UsersList.js
@@ -22,36 +22,35 @@ function UsersList() {
     doCreateUsers();
   };
 
+  // content will show the dynamically loaded users/Errors
+  let content;
   if (isLoadingUsers) {
-    return <Skeleton times={6} className='h-10 w-full' />;
-  }
-
-  if (loadingUsersError) {
-    return <div>Error fetching data...</div>;
-  }
-
-  const renderedUsers = data.map((user) => {
-    return (
-      <div key={user.id} className='mb-2 border rounded'>
-        <div className='flex p-2 justify-between items-center cursor-pointer'>
-          {user.name}
+    content = <Skeleton times={6} className='h-10 w-full' />;
+  } else if (loadingUsersError) {
+    content = <div>Error fetching data...</div>;
+  } else {
+    content = data.map((user) => {
+      return (
+        <div key={user.id} className='mb-2 border rounded'>
+          <div className='flex p-2 justify-between items-center cursor-pointer'>
+            {user.name}
+          </div>
         </div>
-      </div>
-    );
-  });
+      );
+    });
+  }
 
   return (
+    // showing header is permanent
     <div>
-      <div className='flex flex-row justify-between m-3'>
+      <div className='flex flex-row justify-between m-3 items-center'>
         <h1 className='m-2 text-xl'>Users</h1>
-        {isCreatingUser ? (
-          "Creating User"
-        ) : (
-          <Button onClick={handleUserAdd}>+ Add User</Button>
-        )}
+        <Button loading={isCreatingUser} onClick={handleUserAdd}>
+          + Add User
+        </Button>
         {creatingUserError && "Error in creating USer"}
       </div>
-      {renderedUsers}
+      {content}
     </div>
   );
 }
