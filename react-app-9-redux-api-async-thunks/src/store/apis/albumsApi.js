b36ded66871b1c7e0cbea3ee552d5fba87a0e4e5
Shubham228197 <shubham228pandey@gmail.com>
2024-11-04 23:44:39 +0530

    ai suggestion

--- react-app-9-redux-api-async-thunks/src/store/apis/albumsApi.js
+++ react-app-9-redux-api-async-thunks/src/store/apis/albumsApi.js
@@ -5,4 +5,6 @@ when we create an api with this process, in back of code
 it creates a slice and that slice stores tons of date like
 states related to the request we are going gto make with thi API
 */
-export const albumApi = createAPI();
+export const albumApi = createAPI({
+  reducerPath: "albums",
+});
