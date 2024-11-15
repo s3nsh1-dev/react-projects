e5d24cae942a9a76aee2865b4be8b28f96b05de3
Shubham228197 <shubham228pandey@gmail.com>
2024-11-16 03:30:08 +0530

    Introduced Modes

--- react-app-11-ts-color-array/src/App.tsx
+++ react-app-11-ts-color-array/src/App.tsx
@@ -1,28 +1,12 @@
-import { useState } from 'react';
-import ShowColorsArray from './components/ShowColorsArray';
-import { colorsList } from './constants/ListOfColors';
+import OfflineApp from "./components/Offline/OfflineApp";
+import OnlineApp from "./components/OnlineApp";
 
 function App() {
-  const [colorArray, setColorArray] = useState<string[]>([  "Red",
-  "Blue",
-  "Green",]);
-  const handleAddColor = () => {
-    const color: string = colorsList[Math.floor(Math.random() * colorsList.length)];
-    setColorArray([...colorArray, color])
-  }
-  const handleRemoveColor = () =>{
-    /*
-    violets React by directly removing from state
-    setColorArray([...colorArray.pop()]);
-    */
-    // removes immutably
-    setColorArray([...colorArray.slice(0,-1)])
-  }
+
   return (
     <div>
-      <button onClick={handleAddColor}>++Add Color++</button>
-      <button onClick={handleRemoveColor}>--Remove Color--</button>
-      <ShowColorsArray colors = {colorArray}/>
+      {/* <OfflineApp/> */}
+      <OnlineApp/>
     </div>
   );
 }
