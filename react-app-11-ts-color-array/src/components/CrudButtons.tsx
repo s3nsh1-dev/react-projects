480e8c6ec08538c3ff10b30253ec340354a1b944
Shubham228197 <shubham228pandey@gmail.com>
2024-11-16 03:30:24 +0530

    Online CRUD

--- react-app-11-ts-color-array/src/components/CrudButtons.tsx
+++ react-app-11-ts-color-array/src/components/CrudButtons.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { FC } from 'react'
 
-export default function CrudButtons() {
+interface CrudProps {
+}
+const CrudButtons: FC<CrudProps> = () => {
+    const handleColorChange = (type: string) => {
+        if(type === 'add'){
+            console.log("Color Added")
+            
+        }else if(type === 'remove'){
+            console.log("Color Removed")
+            
+        }
+
+    }
   return (
     <div>
-      
+        <button onClick={()=>handleColorChange("add")}>++Add Color++</button>
+        <button onClick={()=>handleColorChange("remove")}>--Remove Color--</button>
     </div>
   )
 }
+
+export default CrudButtons;
