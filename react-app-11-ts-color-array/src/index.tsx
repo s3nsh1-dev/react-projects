511991ac721ff839624cc2ec127090923bcdae21
Shubham228197 <shubham228pandey@gmail.com>
2024-11-16 05:09:56 +0530

    minor update

--- react-app-11-ts-color-array/src/index.tsx
+++ react-app-11-ts-color-array/src/index.tsx
@@ -1,6 +1,11 @@
-import ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from "./App";
+import { MyContextProvide } from "./context/colorContext";
 
 const rootDiv = document.getElementById("root") as HTMLElement;
 const root = ReactDOM.createRoot(rootDiv);
-root.render(<App />);
+root.render(
+  <MyContextProvide>
+    <App />
+  </MyContextProvide>
+);
