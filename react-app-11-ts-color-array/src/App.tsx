ebd3045000d79c193aa6aaf86942a41bf548c404
Shubham228197 <shubham228pandey@gmail.com>
2024-11-15 23:29:40 +0530

    app basics completed

--- react-app-11-ts-color-array/src/App.tsx
+++ react-app-11-ts-color-array/src/App.tsx
@@ -1,8 +1,50 @@
-import React from 'react';
+import { useState } from 'react';
+
+const colors = [
+  "Red",
+  "Blue",
+  "Green",
+  "Yellow",
+  "Orange",
+  "Purple",
+  "Pink",
+  "Brown",
+  "Cyan",
+  "Magenta",
+  "Lime",
+  "Teal",
+  "Indigo",
+  "Violet",
+  "Turquoise",
+  "Gold",
+  "Silver",
+  "Beige",
+  "Lavender",
+  "Peach"
+];
+
 
 function App() {
+  const [colorArray, setColorArray] = useState<string[]>([  "Red",
+  "Blue",
+  "Green",]);
+  const handleAddColor = () => {
+    const color: string = colors[Math.floor(Math.random() * colors.length)];
+    setColorArray([...colorArray, color])
+  }
+  const handleRemoveColor = () =>{
+    /*
+    violets React by directly removeing from state
+    setColorArray([...colorArray.pop()]);
+    */
+    // removes immutably
+    setColorArray([...colorArray.slice(0,-1)])
+  }
   return (
-    <div className="App">TS app
+    <div>
+      <button onClick={handleAddColor}>++Add Color++</button>
+      <button onClick={handleRemoveColor}>--Remove Color--</button>
+      {colorArray.map((color, index)=>{return <div key={index}>{color}</div>})}
     </div>
   );
 }
