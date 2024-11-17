e68e43b08ab18e756606d18011d3a3250d9ffa91
Shubham228197 <shubham228pandey@gmail.com>
2024-11-17 23:31:25 +0530

    kam ki baat

--- react-app-11-ts-color-array/src/components/DisplayFetchedData.tsx
+++ react-app-11-ts-color-array/src/components/DisplayFetchedData.tsx
@@ -1,28 +1,4 @@
-// import React, { FC } from "react";
-// import { useColorContext } from "../hooks/useColorContext";
-
-// interface DataProp {
-//   id: number;
-//   value: string;
-// }
-// interface DataSetProp {
-//   data: DataProp[];
-// }
-
-// const DisplayFetchedData: FC<DataSetProp> = ({ data }) => {
-//   const { stateData } = useColorContext();
-//   console.log("data:", data);
-//   console.log("stateData:", stateData);
-//   const renderFetchedColorData = data.map((color) => {
-//     return <div key={color.id}>{color.value}</div>;
-//   });
-//   return <div>{renderFetchedColorData}</div>;
-// };
-
-// export default DisplayFetchedData;
-import React from "react";
 import { useColorContext } from "../hooks/useColorContext";
-
 interface DataProp {
   id: string; // Ensure this matches your API data
   value: string;
@@ -31,7 +7,7 @@ interface DataProp {
 const DisplayFetchedData = () => {
   // Explicitly cast stateData as DataProp[]
   const { stateData } = useColorContext() as { stateData: DataProp[] };
-
+  console.log("Fetching Data");
   const renderFetchedColorData = stateData.map((color) => {
     return <div key={color.id}>{color.value}</div>;
   });
