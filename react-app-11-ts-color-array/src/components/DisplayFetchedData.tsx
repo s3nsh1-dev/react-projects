3278347151dfbcb656f90d067d33e158ed166755
Shubham228197 <shubham228pandey@gmail.com>
2024-11-17 22:22:27 +0530

    useContest stateData

--- react-app-11-ts-color-array/src/components/DisplayFetchedData.tsx
+++ react-app-11-ts-color-array/src/components/DisplayFetchedData.tsx
@@ -1,22 +1,42 @@
-import React, { FC } from 'react'
+// import React, { FC } from "react";
+// import { useColorContext } from "../hooks/useColorContext";
+
+// interface DataProp {
+//   id: number;
+//   value: string;
+// }
+// interface DataSetProp {
+//   data: DataProp[];
+// }
+
+// const DisplayFetchedData: FC<DataSetProp> = ({ data }) => {
+//   const { stateData } = useColorContext();
+//   console.log("data:", data);
+//   console.log("stateData:", stateData);
+//   const renderFetchedColorData = data.map((color) => {
+//     return <div key={color.id}>{color.value}</div>;
+//   });
+//   return <div>{renderFetchedColorData}</div>;
+// };
+
+// export default DisplayFetchedData;
+import React from "react";
+import { useColorContext } from "../hooks/useColorContext";
+
 interface DataProp {
-    id: number,
-    value: string,
-}
-interface DataSetProp {
-    data: DataProp[],
+  id: string; // Ensure this matches your API data
+  value: string;
 }
 
-const DisplayFetchedData: FC<DataSetProp> = ({data}) => {
-    const renderFetchedColorData = data.map ((color)=>{
-            return (<div key={color.id}>{color.value}</div>)
-        })
-  return (
-    <div>
-      {renderFetchedColorData}
-    </div>
-  )
-}
+const DisplayFetchedData = () => {
+  // Explicitly cast stateData as DataProp[]
+  const { stateData } = useColorContext() as { stateData: DataProp[] };
 
-export default DisplayFetchedData
+  const renderFetchedColorData = stateData.map((color) => {
+    return <div key={color.id}>{color.value}</div>;
+  });
 
+  return <div>{renderFetchedColorData}</div>;
+};
+
+export default DisplayFetchedData;
