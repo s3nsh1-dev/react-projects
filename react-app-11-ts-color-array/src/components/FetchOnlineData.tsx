0ee356c01e1da9a8ed98ba8b662c41ec6ecab058
Shubham228197 <shubham228pandey@gmail.com>
2024-11-17 22:23:32 +0530

    Hera pheri

--- react-app-11-ts-color-array/src/components/FetchOnlineData.tsx
+++ react-app-11-ts-color-array/src/components/FetchOnlineData.tsx
@@ -4,14 +4,13 @@ import { fetchColors } from "../api/fetchColors";
 import { useColorContext } from "../hooks/useColorContext";
 
 interface colorProp {
-  id: number;
+  id: string;
   value: string;
 }
 
 export default function FetchOnlineData() {
   const [result, setResult] = useState<colorProp[]>([]);
-  const ContextValue = useColorContext();
-  console.log("Context Value", ContextValue?.data);
+  const { stateData, handleColorState } = useColorContext();
   /*
   use this context state update it and use it to display the
   global colors state in Homepage.
@@ -22,16 +21,18 @@ export default function FetchOnlineData() {
       try {
         const fetching = await fetchColors();
         setResult(await fetching);
-        console.log("Data Fetched: ", fetching);
+        handleColorState(await fetching); // update global colors state in context. This is just an example. You can use it as per your requirement.
+        console.log("Fetching Data");
       } catch (err) {
         console.log("Fetching Error: ", err);
       }
     };
     fetchingData();
   }, []);
+
   return (
     <div>
-      <DisplayFetchedData data={result} />
+      <DisplayFetchedData />
     </div>
   );
 }
@@ -46,4 +47,12 @@ Instead, they inherit this from their enclosing lexical context
 
 Arrow functions work well in React because they don't require explicit
 binding of {this} (since they inherit it from the enclosing context).
+*/
+
+/*
+Component Flow
+Due to JS being Asynchronous in nature it fist executes all the
+functions the call the useEffect which cause another render
+and the function that is executed will loading React app run
+twice
 */
