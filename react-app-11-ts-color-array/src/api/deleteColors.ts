2e1305903d6d88ebe2ce9b38e3eb0ab439660378
Shubham228197 <shubham228pandey@gmail.com>
2024-11-18 23:06:05 +0530

    line

--- react-app-11-ts-color-array/src/api/deleteColors.ts
+++ react-app-11-ts-color-array/src/api/deleteColors.ts
@@ -1,12 +1,13 @@
 import axios from "axios";
+import { fetchColors } from "./fetchColors";
 
 const deleteColors = async () => {
-    const dbData = await axios.get("http://localhost:3001/colors");
-    const dataItems = await dbData.data;
-    const lastItemId = dataItems[dataItems.length -1].id
-    await axios.delete(`http://localhost:3001/colors/${lastItemId}`)
-  const fetchResponse = await axios.get("http://localhost:3001/colors")
-    return fetchResponse.data;
-}
+  console.log("Deleting Colors");
+  const dbData = await axios.get("http://localhost:3001/colors");
+  const dataItems = await dbData.data;
+  const lastItemId = dataItems[dataItems.length - 1].id;
+  await axios.delete(`http://localhost:3001/colors/${lastItemId}`);
+  return await fetchColors();
+};
 
-export default deleteColors
+export default deleteColors;
