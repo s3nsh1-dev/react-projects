a8e0a97ee2fa2ece93550a5a209096988a64fa67
Shubham228197 <shubham228pandey@gmail.com>
2024-11-18 23:57:37 +0530

    wip

--- react-app-11-ts-color-array/src/components/DisplayFetchedData.tsx
+++ react-app-11-ts-color-array/src/components/DisplayFetchedData.tsx
@@ -1,4 +1,4 @@
-import { useColorContext } from "../hooks/useColorContext";
+import { useColorState } from "../hooks/useColorState";
 import { useFetchColors } from "../hooks/useFetchColors";
 interface DataProp {
   id: string; // Ensure this matches your API data
@@ -9,7 +9,7 @@ const DisplayFetchedData = () => {
   useFetchColors();
   // Explicitly cast stateData as DataProp[]
   console.log("DisplayFetchedData.tsx");
-  const { stateData } = useColorContext() as { stateData: DataProp[] };
+  const { stateData } = useColorState() as { stateData: DataProp[] };
   const renderFetchedColorData = stateData.map((color) => {
     return <div key={color.id}>{color.value}</div>;
   });
