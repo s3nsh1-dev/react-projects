724c37c9a30c0723207faa3bc6524442dfafa612
Shubham228197 <shubham228pandey@gmail.com>
2024-11-18 23:06:12 +0530

    code

--- react-app-11-ts-color-array/src/api/fetchColors.ts
+++ react-app-11-ts-color-array/src/api/fetchColors.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const fetchColors = async () => {
-    const response =  await axios.get("http://localhost:3001/colors")
-    return response.data;
-}
+  const response = await axios.get("http://localhost:3001/colors");
+  console.log("Alone Fetched Colors");
+  return response.data;
+};
