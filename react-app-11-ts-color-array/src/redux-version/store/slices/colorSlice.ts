14713946658ceb1315dd03a99a9ee38f1cf8af86
Shubham228197 <shubham228pandey@gmail.com>
2024-11-19 04:20:15 +0530

    BREAK

--- react-app-11-ts-color-array/src/redux-version/store/slices/colorSlice.ts
+++ react-app-11-ts-color-array/src/redux-version/store/slices/colorSlice.ts
@@ -2,13 +2,28 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const colorSlice = createSlice({
   name: "colors",
-  initialState: [],
+  initialState: {
+    isLoading: false,
+    colorHistory: [],
+    error: null,
+  },
   reducers: {
-    addColor: (state, action) => {},
-    fetchColors: (state, action) => {},
-    deleteColor: (state, action) => {},
+    addColor: (state, action) => {
+      state.colorHistory = action.payload; // -> for now
+      //   state.colorHistory.push(action.payload}); // -> preferred
+    },
+    fetchColors: (state, action) => {
+      state.colorHistory = action.payload;
+    },
+    deleteColor: (state, action) => {
+      state.colorHistory = action.payload; // -> for now
+      // preferred way to implement deleteColor
+      //   state.colorHistory = state.colorHistory.filter(
+      //     (color) => color.id !== action.payload
+      //   );
+    },
   },
 });
 
 export const { addColor, fetchColors, deleteColor } = colorSlice.actions;
-export const { colorReducer } = colorSlice.reducer;
+export const colorReducer = colorSlice.reducer;
