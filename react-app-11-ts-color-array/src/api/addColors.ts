f6b0a7f2a7583a7f00b43d128604b720ff0c4012
Shubham228197 <shubham228pandey@gmail.com>
2024-11-18 23:05:59 +0530

    short

--- react-app-11-ts-color-array/src/api/addColors.ts
+++ react-app-11-ts-color-array/src/api/addColors.ts
@@ -1,11 +1,12 @@
 import { faker } from "@faker-js/faker";
 import axios from "axios";
+import { fetchColors } from "./fetchColors";
 
 export const addColors = async () => {
-    await axios.post("http://localhost:3001/colors",{
-                id: faker.string.alphanumeric(10),
-                value: faker.color.human()
-            })
-    const fetchResponse = await axios.get("http://localhost:3001/colors")
-    return fetchResponse.data;
-}
+  console.log("Adding Colors");
+  await axios.post("http://localhost:3001/colors", {
+    id: faker.string.alphanumeric(10),
+    value: faker.color.human(),
+  });
+  return fetchColors();
+};
