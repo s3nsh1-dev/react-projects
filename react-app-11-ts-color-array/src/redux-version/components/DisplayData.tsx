df7dd8bf513602363aa08ab2c8fe21e5a3747b35
Shubham228197 <shubham228pandey@gmail.com>
2024-11-19 23:58:13 +0530

    faded memo useSelect

--- react-app-11-ts-color-array/src/redux-version/components/DisplayData.tsx
+++ react-app-11-ts-color-array/src/redux-version/components/DisplayData.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const DisplayData = () => {
+  const colors = useSelector;
   return <div style={{ fontWeight: "bold" }}>DB data</div>;
 };
 
