b39e259ee0e90eb871613417543f9fadd4cc329e
Shubham228197 <shubham228pandey@gmail.com>
2024-11-20 23:03:42 +0530

    typo

--- react-app-11-ts-color-array/src/redux-version/store/thunks/postData.ts
+++ react-app-11-ts-color-array/src/redux-version/store/thunks/postData.ts
@@ -4,7 +4,8 @@ import { faker } from "@faker-js/faker";
 export const postData = async () => {
   const response = await axios.post("http://localhost:3001/colors", {
     id: faker.string.alphanumeric(4),
-    value: faker.color,
+    value: faker.color.human(),
   });
+  console.log("Data Posted Successfully", response.data);
   return response.data;
 };
