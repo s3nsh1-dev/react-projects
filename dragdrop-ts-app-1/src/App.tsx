59693e3b25d9aa344a3e455e9dd5faecfc83c50b
Shubham Pandey <shubham228pandey@gmail.com>
2024-12-26 23:23:18 +0530

    Ts section 9 is not a react app

--- dragdrop-ts-app-1/src/App.tsx
+++ dragdrop-ts-app-1/src/App.tsx
@@ -1,43 +1,5 @@
 const App = () => {
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    // Add your code here to add the new project to the list
-  };
-
-  return (
-    <div>
-      <h1>There is our App TS</h1>
-      <div id='project-input'>
-        <form onSubmit={handleSubmit}>
-          <div className='form-control'>
-            <label htmlFor='title'>Title: </label>
-            <input type='text' id='title' />
-          </div>
-          <div className='form-control'>
-            <label htmlFor='description'>Description: </label>
-            <textarea id='description'></textarea>
-          </div>
-          <div className='form-control'>
-            <label htmlFor='people'>People: </label>
-            <input type='number' id='people' step='1' min='0' max='10' />
-          </div>
-          <button type='submit'>ADD PROJECT</button>
-        </form>
-      </div>
-      <div id='single-project'>
-        <li></li>
-      </div>
-      <div id='project-list'>
-        <section className='projects'>
-          <header>
-            <h2></h2>
-          </header>
-          <ul></ul>
-        </section>
-      </div>
-      <div id='app'></div>
-    </div>
-  );
+  return <div> I need to working on this project separately</div>;
 };
 
 export default App;
